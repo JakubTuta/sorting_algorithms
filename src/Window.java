d933939135eca54b33a80a653894db99f76c000f
JakubTuta <jakubtuta@o2.pl>
2023-02-26 15:58:59 +0100

    got screen width and height

--- src/Window.java
+++ src/Window.java
@@ -2,7 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Window extends JPanel{
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    final int SCREEN_WIDTH = (int)screenSize.getWidth();
+    final int SCREEN_HEIGHT = (int)screenSize.getHeight();
     Window() {
-        this.setBackground(Color.green);
+        this.setBackground(Color.gray);
     }
 }
