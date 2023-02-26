ea717d4188f2ddbd0d4075ebc70468b7a3ea6111
JakubTuta <jakubtuta@o2.pl>
2023-02-26 16:58:58 +0100

    array of numbers setup

--- src/Window.java
+++ src/Window.java
@@ -1,11 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
 
 public class Window extends JPanel{
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    final int SCREEN_WIDTH = (int)screenSize.getWidth();
-    final int SCREEN_HEIGHT = (int)screenSize.getHeight();
+    final double SCREEN_WIDTH = screenSize.getWidth();
+    final double SCREEN_HEIGHT = screenSize.getHeight();
+    final int numOfNumbers = 512;
+    int[] arrOfNumbers = new int[numOfNumbers];
+    final double tileWidth = SCREEN_WIDTH / numOfNumbers;
+    double tileHeight = 0;
     Window() {
         this.setBackground(Color.gray);
+        for(int i = 0; i < 512; i++) {
+            arrOfNumbers[i] = i + 1;
+        }
+        tileHeight = SCREEN_HEIGHT / Arrays.stream(arrOfNumbers).max().getAsInt();
     }
 }
