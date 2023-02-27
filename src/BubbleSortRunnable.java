8fca527a2f46ab4ce8df611dc9d8603736717312
JakubTuta <jakubtuta@o2.pl>
2023-02-27 22:38:52 +0100

    Bubble sort is working fine

---
+++ src/BubbleSortRunnable.java
@@ -0,0 +1,36 @@
+public class BubbleSortRunnable implements Runnable {
+    private final int[] arr;
+    private final Window window;
+    final int DELAY = 50;
+
+    public BubbleSortRunnable(int[] arr, Window window) {
+        this.arr = arr;
+        this.window = window;
+    }
+
+    @Override
+    public void run() {
+        int i, j;
+        boolean swapped;
+        for (i = 0; i < arr.length - 1; i++) {
+            swapped = false;
+            for (j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swapped = true;
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    window.repaint();
+                    try {
+                        Thread.sleep(DELAY);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            if (!swapped)
+                break;
+        }
+    }
+}
